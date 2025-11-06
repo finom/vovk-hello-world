404d789beedcc26cafba8d25bbf08416bade4291
Andrey Gubanov <andrii@gubanov.eu>
2025-11-06 23:27:34 +0400

    fix: Minor change to streamTokens

--- src/modules/stream/StreamService.ts
+++ src/modules/stream/StreamService.ts
@@ -5,12 +5,12 @@ export default class StreamService {
   static async *streamTokens() {
     const tokens: VovkIteration<typeof StreamController.streamTokens>[] =
       "Vovk.ts is a RESTful back-end meta-framework with RPC built on top of Next.js App Router, and this text is a JSONLines stream demo."
-        .match(/[^\s]+(?:\s+)?/g)
+        .match(/[^\s-]+-?(?:\s+)?/g)
         ?.map((message) => ({ message })) || [];
 
     for (const token of tokens) {
       yield token;
-      await new Promise((resolve) => setTimeout(resolve, 300));
+      await new Promise((resolve) => setTimeout(resolve, 200));
     }
   }
 }
