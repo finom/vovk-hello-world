8ef1aa1bfddad73d59721a6eb4a82056d83148c5
Andrey Gubanov <andrii@gubanov.eu>
2025-11-06 23:14:23 +0400

    fix: Reduce amount of tokens at streamTokens handler

--- src/modules/stream/StreamService.ts
+++ src/modules/stream/StreamService.ts
@@ -4,13 +4,13 @@ import type StreamController from "./StreamController";
 export default class StreamService {
   static async *streamTokens() {
     const tokens: VovkIteration<typeof StreamController.streamTokens>[] =
-      "Vovk.ts is a RESTful back-end meta-framework with RPC built on top of Next.js App router, and this text is a JSONLines stream demo."
-        .match(/[^\s.-]+|\s+|\.|-/g)
+      "Vovk.ts is a RESTful back-end meta-framework with RPC built on top of Next.js App Router, and this text is a JSONLines stream demo."
+        .match(/[^\s]+(?:\s+)?/g)
         ?.map((message) => ({ message })) || [];
 
     for (const token of tokens) {
       yield token;
-      await new Promise((resolve) => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 300));
     }
   }
 }
