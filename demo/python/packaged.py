84b0382a2841f320fad359ebd3d4faff7b12bc67
Andrey Gubanov <andrii@gubanov.eu>
2025-11-07 10:58:27 +0400

    fix: Remove comments from python demo

--- demo/python/packaged.py
+++ demo/python/packaged.py
@@ -12,7 +12,6 @@ def main() -> None:
     }
     query: UserRPC.UpdateUserQuery = {"notify": "email"}
     params: UserRPC.UpdateUserParams = {"id": "123e4567-e89b-12d3-a456-426614174000"}
-    # Update user using local module
     update_user_response = UserRPC.update_user(
         params=params,
         body=body,
@@ -20,11 +19,9 @@ def main() -> None:
     )
     print('UserRPC.update_user:', update_user_response)
     
-    # Get OpenAPI spec from local module
     openapi_response = OpenApiRPC.get_spec()
     print(f"OpenApiRPC.get_spec from the local module: {openapi_response['info']['title']} {openapi_response['info']['version']}")
     
-    # Stream tokens from local module
     stream_response = StreamRPC.stream_tokens()
     print("streamTokens:")
     for item in stream_response:
